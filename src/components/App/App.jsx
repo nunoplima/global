a7d381b3434b6617ee76b28b4d60406de2a787a0
nuno <ngplbc@gmail.com>
2020-06-20 11:35:49 +0100

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "../Home/Home";
+import Employees from "../Employees/Employees";
+import EmployeeForm from "../EmployeeForm/EmployeeForm";
+import NotFound from "../NotFound/NotFound";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import "./App.css";
+
+const App = () => {
+
+    return (
+		<>
+			<NavBar />
+			<main className="container">
+				<Switch>
+					<Route path="/employees/:employeeId" component={EmployeeForm}/>
+					<Route path="/employees" component={Employees} />
+					<Route path="/home" component={Home} />
+					<Route path="/not-found" component={NotFound} />
+					<Redirect from="/" exact to="/home" />
+					<Redirect to="/not-found" />
+				</Switch>
+			</main>
+			<Footer />
+		</>
+    );
+}
+
+export default App;
